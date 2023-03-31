20182926b369f41c0dbfe6dfa4af4c4c99a4d4a7
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-04-01 00:41:08 +0600

    server created

--- Server.js
+++ Server.js
@@ -8,18 +8,18 @@ const cors = require("cors");
 const routes = require("./routes/ToDoRoute");
 
 const app = express();
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 6000;
 
 app.use(express.json());
 app.use(cors());
 
 mongoose
-    .connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log("Mongodb Connected..."))
-    .catch((err) => console.error(err));
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Mongodb Connected..."))
+  .catch((err) => console.error(err));
 
 // Routes
 app.use(routes);
